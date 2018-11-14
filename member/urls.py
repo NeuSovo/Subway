67cea8b9d47762907d2014d8a352d2001802381d
shrine <shrine8263@yeah.net>
2018-11-14 23:47:41 +0800

    xian

--- member/urls.py
+++ member/urls.py
@@ -7,9 +7,7 @@ urlpatterns = [
     path('', DeptListView.as_view()),
     path('auth/login', LoginView.as_view(), name='login'),
     path('auth/logout', logout_view, name='logout'),
-
     path('global/qrcode/<path:data>', qrcode_view, name='qrcode'),
-
     path('member/member_add', MemberAddView.as_view(), name='member_add'),
     path('member/member_list', MemberListView.as_view(), name='member_list'),
     path('member/member_list/<int:dept_id>', MemberListView.as_view(), name='member_list_with_id'),
@@ -17,7 +15,6 @@ urlpatterns = [
     path('member/member_export', export_member_data, name='member_export'),
     path('member/member_export/<int:dept_id>', export_member_data, name='member_export_with_id'),
     path('member/member_detail/<str:pk>', MemberDetailView.as_view(), name='member_detail'),
-
     path('member/dept', DeptListView.as_view(), name='dept_list'),
     path('member/dept_create',DeptCreateView.as_view(), name='dept_create'),
     path('member/dept_assign_account/<int:dept_id>', AssignAccountView.as_view(), name='dept_assign_account'),
