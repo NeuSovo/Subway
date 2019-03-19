4ad59c95d89bd35b68b5104f8f277eb212d0b803
shrine <shrine8263@yeah.net>
2019-03-19 20:16:07 +0800

    update

---
+++ device/student.sql
@@ -0,0 +1,3 @@
+﻿select majorname, count(student.majorid) 'count'
+from major
+       left join student on student.majorid = major.majorid group by major.majorid
--- device/tests.py
+++ device/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+import time
 
 # Create your tests here.
+a = 1000000
+b = 1
+count = 0
+time1 = time.time()
+for i in range(a):
+    if (i % 100 == 0):
+        b = 1.0
+        count += 1
+    b = b * 1.5
+
+print(time.time() - time1)
+print(b,count)
